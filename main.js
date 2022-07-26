10b117b4fb1dd5329e3355856a124ae370cc549f
Andres <andreslindo20@gmail.com>
2022-07-25 22:34:21 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 document.querySelector('#getButton').addEventListener("click", apiRequest)
 
 async function apiRequest(){
-    const alienName = document.querySelector('input').value
+
+    
+    const pokemonName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://star-trek-api-2000.herokuapp.com/api/${alienName}`)
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName} `)
         const data = await response.json()
         console.log(data)
-        document.getElementById('alienName').innerText = data.speciesName
-        document.getElementById('alienWorld').innerText = data.homeworld
-        document.getElementById('alienFeatures').innerText = data.features
-        document.getElementById('alienFacts').innerText = data.interestingFact
-        document.getElementById('alienExamples').innerText = data.notableExamples
 
-        document.getElementById('alienImage').src = data.image
-        document.getElementById('alienCaption').innerText = data.speciesName
+
+        document.getElementById('pokemonName').innerText = data.species.name
+        document.getElementById('pokemonWeight').innerText = data.weight
+        document.getElementById('pokemonHeight').innerText = data.height
+        document.getElementById('pokemonType').innerText = data.types[0].type.name
+        document.getElementById('pokemonAttack').innerText = data.moves[0].move.name
+        document.getElementById('pokemonImage').src = data.sprites.other.home.front_default
+        document.getElementById('pokemonCaption').innerText = data.name
     }catch(error) {
         console.log(error)
     }
